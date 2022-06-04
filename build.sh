dea6e60eed837e750fca25dd7ac5fe4d97647e73
John Daiker <daikerjohn@gmail.com>
2022-06-04 13:55:44 -0700

    Scripts for installing prereqs and building

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ARCH=$(uname -i)
+source /etc/os-release
+
+if [[ ! -e "../travellingCamera/daemon/lib/$ARCH/$VERSION_CODENAME" ]]; then
+  mkdir -p ../travellingCamera/daemon/lib/$ARCH/$VERSION_CODENAME
+fi
+
+mkdir bld
+cd bld
+cmake -DBUILD_GSTREAMER_PLUGIN=ON -DBUILD_JNI=OFF -DBUILD_DEPENDENCIES=OFF ..
+cd bld
+make -j3
+if [[ "$?" == "0" ]]; then
+  cp ./dependency/libkvscproducer/kvscproducer-src/libcproducer.so ../../travellingCamera/daemon/lib/$ARCH/$VERSION_CODENAME/
+  cp ./libKinesisVideoProducer.so ../../travellingCamera/daemon/lib/$ARCH/$VERSION_CODENAME/
+  cp ./libgstkvssink.so ../../travellingCamera/daemon/lib/$ARCH/$VERSION_CODENAME/
+fi
+
---
+++ prereqs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt-get install libssl-dev libcurl4-openssl-dev liblog4cplus-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-base-apps gstreamer1.0-plugins-bad gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-tools cmake m4 git build-essential
